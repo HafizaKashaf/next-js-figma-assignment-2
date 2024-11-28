ab35b1b1fd8648103708ea7cd2ff64aa5c0bf03b
HafizaKashaf <159634597+HafizaKashaf@users.noreply.github.com>
2024-11-28 19:25:33 +0500

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Logo = () => {
   return (
@@ -8,12 +9,12 @@ const Logo = () => {
          <h1 className='font-roboto font-bold leading-[27px] text-[18px] w-[327px] h-[54px] md:lg:hidden'>Trusted by the world's best companies [social proof to build credibility]</h1>
          <h1 className='w-[320px] h-[68px] font-roboto font-bold text-[24px] leading-[33.6px] text-[rgb(0,0,0)] max-sm:hidden'>Trusted by 2000+ companies worldwide.</h1>
          <div className='w-[880px] h-[56px] pt-[8.74px] pb-[8.74px] flex gap-[19.12px] items-center max-sm:h-[65.34px]'>
-            <img src="/images/logo1.png" alt="logo1" className='w-[123.8px] h-[38.52px] max-sm:w-[103px] max-sm:h-[33px]'/>
-            <img src="/images/logo2.png" alt="logo2" className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px] ' />
-            <img src="/images/logo3.png" alt="logo3" className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' /> 
-            <img src="/images/logo4.png" alt="logo4" className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' />
-            <img src="/images/logo5.png" alt="logo5" className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' />
-            <img src="/images/logo6.png" alt="logo6" className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' />        
+           <Image src="/images/logo1.png" alt="logo1" width={123.8} height={38.52} className='w-[123.8px] h-[38.52px] max-sm:w-[103px] max-sm:h-[33px]'/>
+            <Image src="/images/logo2.png" alt="logo2" width={123.8} height={38.52} className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px] ' />
+            <Image src="/images/logo3.png" alt="logo3" width={123.8} height={38.52} className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' /> 
+            <Image src="/images/logo4.png" alt="logo4" width={123.8} height={38.52} className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' />
+            <Image src="/images/logo5.png" alt="logo5" width={123.8} height={38.52} className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' />
+            <Image src="/images/logo6.png" alt="logo6" width={123.8} height={38.52} className='w-[123.8px] h-[38.52px] max-sm:w-[103px]  max-sm:h-[33px]' />          
             </div>
      </div>
     </div>
